ec97b46fa61ef2ec905d6e47db7619a53d9bb436
Veronica <terrence_bogard@hotmail.com>
2021-03-15 21:27:48 +0100

    Hola

---
+++ pruebas/Animal.java
@@ -0,0 +1,42 @@
+package pruebas;
+
+import java.time.LocalDate;
+
+public class Animal {
+	// atributos
+	String nombre;
+	LocalDate fecha;
+	
+	// metodos
+
+	public Animal(String nombre, LocalDate fecha) {
+		this.nombre = nombre;
+		this.fecha = fecha;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public LocalDate getFecha() {
+		return fecha;
+	}
+
+
+
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
