97171e7f97422e77143ce1ee805eb1bbe3b7e4c5
Veronica <terrence_bogard@hotmail.com>
2021-03-16 23:41:46 +0100

    a

--- pruebas/EjerExcep3.java
+++ pruebas/EjerExcep3.java
@@ -1,5 +1,7 @@
 package pruebas;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class EjerExcep3 {
@@ -9,14 +11,64 @@ public class EjerExcep3 {
 		
 		Scanner input = new Scanner(System.in);
 		
-		String teclado;
+		String teclado, comando;
+		Integer ca = null, cb = null;
+		//ArrayList<String> comando = new ArrayList<>();
 		
-		do {
-			System.err.println(">");
+		System.out.println(">");
+		teclado = input.nextLine();
+		
+		while (!teclado.equalsIgnoreCase("f")) {
+			
+			comando = teclado.substring(0,1);
+			//comando = (ArrayList<String>) Arrays.asList(teclado.split(" "));
+			switch (comando.toUpperCase()) {
+				case "A":
+					try {
+						ca = Integer.parseInt(teclado.substring(1,teclado.length()).trim());
+						System.out.println("Cateto A: " + ca);
+					} catch (NumberFormatException e) {
+						System.err.println("Error al introducir valor del cateto");
+					} finally {
+						break;
+					}
+				case "B":
+					try {
+						cb = Integer.parseInt(teclado.substring(1,teclado.length()).trim());
+						System.out.println("Cateto B: " + cb);
+					} catch (NumberFormatException e) {
+						System.err.println("Error al introducir valor del cateto");
+					} finally {
+						break;
+					}
+				case "C":
+					try {
+						if (ca == null || cb == null) {
+							throw new NullPointerException();
+						}
+						System.out.println("Hipotenusa: " + calcula(ca,cb));
+						ca=null;
+						cb=null;
+					} catch (NullPointerException e) {
+						System.err.println("ERROR. Establezca el valor de los catetos. A: " + ca + ", B: " + cb);
+					} catch (Exception e) {
+						System.err.println(e);
+					} finally {
+						break;
+					}
+				default:
+					System.err.println("Introduzca un comando válido o \"F\" para salir");
+			}
+			
+			System.out.println(">");
 			teclado = input.nextLine();
-		} while (!teclado.equalsIgnoreCase("fin"));
-
+		}
 		input.close();
 	}
-
+	
+	public static double calcula(int ca, int cb) {
+		double h;
+		h = (Math.pow(ca, 2) + Math.pow(cb, 2));
+		return h;
+	}
 }
